998054b0683d2dd280bdf150094dec2ec9dd0e69
Herodion <momohhero2@gmail.com>
2019-10-09 21:10:46 +0100

    Added the 'Chat Module' and 'Assistant Module' and linked the 'Admin' button on the 'Assistant Module' to the 'Dashboard'

---
+++ app/src/main/java/com/softwarechapel/chatbotsis/PersonalAssistantHome.java
@@ -0,0 +1,30 @@
+package com.softwarechapel.chatbotsis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PersonalAssistantHome extends AppCompatActivity {
+
+    Button adminButton;
+    Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_assistant_home);
+
+        adminButton = findViewById(R.id.admin_button_card);
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, DashboardActivity.class));
+                finish();
+            }
+        });
+    }
+}
